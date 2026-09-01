786fc4885934045a9860530af5b002c2d1854bff
duckyducky2850 <derpyduckalotl@gmail.com>
2026-09-01 22:24:38 +0200

    TTAS lock

--- TTASLock.java
+++ TTASLock.java
@@ -1,18 +1,28 @@
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.locks.Lock;
 
-public class TTASLock {
-    AtomicBoolean state = new AtomicBoolean(false);
+public class TTASLock 
+{
+
+    private final AtomicBoolean locked = new AtomicBoolean(false);
+
+    /* Do not modify this method */
+    private boolean testAndSet() 
+    {
+        return locked.getAndSet(true);
+    }
+
     public void lock() {
     while (true) {
-        while (state.get()) {};
-            if (!state.getAndSet(true))
+        while (locked.get()) {};
+            if (!testAndSet())
                 return;
     }
+
     }
     public void unlock() {
-        state.set(false);
+        locked.set(false);
     }
+    
 }
 
 
